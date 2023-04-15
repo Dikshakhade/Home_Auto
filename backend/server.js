d146cd73c288844d152ad5356749f4eae445582e
Dikshakhade <dikshakhade305@gmail.com>
2023-04-15 19:15:04 +0530

    no error code

--- backend/server.js
+++ backend/server.js
@@ -23,18 +23,18 @@ app.use("/users", userRoute);
 app.use("/users", homeRoute);
 
 app.get("/", (req, res) => {
-  res.send("api is running on port 5000");
+  res.send("api is running on port 5001");
 });
 
-// app.listen(5001, console.log("api is running"));
+app.listen(5001, console.log("api is running"));
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-server.listen(5001, () => {
-  console.log("Server is running");
-});
+// server.listen(5001, () => {
+//   console.log("Server is running");
+// });
