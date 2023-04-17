557e2418d0d1ca40af830db06895be089049b19a
AbhishekKhande <abhi23khande@gmail.com>
2023-04-17 18:42:50 +0530

    Static serving of files

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
+const path = require("path");
 const http = require("http");
 const { Server } = require("socket.io");
 
 app.use(express.json());
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 // const server = http.createServer(app);
 const connectdb = require("./config/database");
 
@@ -22,11 +26,6 @@ const homeRoute = require("./routes/homeRoutes");
 app.use("/users", userRoute);
 app.use("/users", homeRoute);
 
-// app.get("/", (req, res) => {
-//   res.send("api is running on port 5001");
-// });
-
-// app.listen(5001, console.log("api is running"));
 const PORT = process.env.PORT;
 const server = app.listen(PORT, console.log(`Server Running on PORT ${PORT}`));
 
@@ -41,13 +40,3 @@ io.on("connection", (socket) => {
     console.log(data);
   });
 });
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// server.listen(5001, () => {
-//   console.log("Server is running");
-// });
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,38 +3,22 @@ import React, { useEffect, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import "./home.css";
 import io from "socket.io-client";
+const socket = io.connect("http://localhost:4000");
+
 function Home() {
   const flag = false;
   const [state, setState] = useState(0);
-  const socket = io.connect("http://localhost:5000");
   const handler = () => {
-    socket.emit("send_message", { message: 0 });
+    socket.emit("send_message", { message: state });
   };
   useEffect(() => {
-    async function fetchData() {
-      console.log(`beg value of state = ${state}`);
-      const config = {
-        headers: {
-          "Content-type": "application/json",
-        },
-      };
-      return await axios
-        .post("/users/home", { state }, config)
-        .then((res) => {
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-    fetchData();
     console.log(`end value of state= ${state}`);
   }, [state]);
 
   return (
     <div>
       <h1>Hello Diksha....</h1>
-      <button onClick={handler}>clickkkkkk</button>
+      {/* <button onClick={handler}>clickkkkkk</button> */}
       <Form>
         <Form.Check
           type="switch"
@@ -46,6 +30,7 @@ function Home() {
           onChange={(e) => {
             if (e.target.checked) setState(1);
             else setState(0);
+            handler();
           }}
         />
       </Form>
